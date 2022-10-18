2f861bdedf74cdb2afea6e726654caee55335204
acquiescent <54900535+acquiescent@users.noreply.github.com>
2022-10-18 14:46:33 +0800

    Minor alignment adjustments

--- Program.cs
+++ Program.cs
@@ -129,11 +129,11 @@ namespace WebApiClient
             String createTableQuery = @"
             IF NOT EXISTS (SELECT * FROM sysobjects WHERE Name = 'weather_records')
                 CREATE TABLE weather_records (
-                    RecordID    INT             IDENTITY(1, 1)  PRIMARY KEY,
-                    Area        VARCHAR(255)                    NOT NULL,
-                    Forecast    VARCHAR(255)                    NOT NULL,
-                    SqlStartTime   DATETIME                     NOT NULL,
-                    SqlEndTime     DATETIME                     NOT NULL
+                    RecordID        INT             IDENTITY(1, 1)  PRIMARY KEY,
+                    Area            VARCHAR(255)    NOT NULL,
+                    Forecast        VARCHAR(255)    NOT NULL,
+                    SqlStartTime    DATETIME        NOT NULL,
+                    SqlEndTime      DATETIME        NOT NULL
                 );
             ";
             ExecuteQuery(connection, createTableQuery);
